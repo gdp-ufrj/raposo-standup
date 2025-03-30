3aadd87a3086d641aa7ad7b1f91c1155221ca471
Lucas Araujo Carvalho <lucasaraujo180701@hotmail.com>
2025-03-30 15:02:29 -0300

    adicionando traducao basica novamente

--- Scripts/OptionsMenu.gd
+++ Scripts/OptionsMenu.gd
@@ -1,7 +1,8 @@
 extends Control
 
-@onready var resolution_button = $VBoxContainer/Resolution/OptionButton as OptionButton
-@onready var resolution_container = $VBoxContainer/Resolution
+@onready var resolution_button = $MarginContainer/VBoxContainer/Resolution/OptionButton as OptionButton
+@onready var resolution_container = $MarginContainer/VBoxContainer/Resolution
+@onready var language_button = $MarginContainer/VBoxContainer/Language/OptionButton as OptionButton
 @onready var back_button = $Button
 
 const RESOLUTION_DICTIONARY : Dictionary = {
@@ -10,30 +11,52 @@ const RESOLUTION_DICTIONARY : Dictionary = {
 	"1440 x 810" : Vector2i(1440, 810)
 }
 
+const LANGUAGES_DICTIONARY : Dictionary = {
+	"Portugues": "pt",
+	"English" : "eng"
+}
+
 func _ready():
 	if OS.get_name() == "Web":
 		resolution_container.hide()
 		back_button.grab_focus()
 	else:
-		resolution_button.item_selected.connect(on_resolution_selected)
+		resolution_button.item_selected.connect(_on_resolution_selected)
 		add_resolution_items()
 		var window_size = get_window().size
 		for i in range(RESOLUTION_DICTIONARY.values().size()):
 			if RESOLUTION_DICTIONARY.values()[i] == window_size:
 				resolution_button.selected = i
 		resolution_button.grab_focus()
+		
+	language_button.item_selected.connect(_on_language_selected)
+	add_language_items()
+	for i in range(LANGUAGES_DICTIONARY.values().size()):
+			if LANGUAGES_DICTIONARY.values()[i] == SettingsData.get_current_language():
+				language_button.selected = i 
 
 func add_resolution_items():
 	for resolution_option in RESOLUTION_DICTIONARY:
 		resolution_button.add_item(resolution_option)
 
-func on_resolution_selected(index : int) -> void:
+func add_language_items():
+	for language_option in LANGUAGES_DICTIONARY:
+		language_button.add_item(language_option)
+
+func _on_resolution_selected(index : int) -> void:
 	var window_size = RESOLUTION_DICTIONARY.values()[index]
 	DisplayServer.window_set_size(window_size)
 	get_window().move_to_center()
 	SettingsData.on_resolution_changed(window_size)
 
+func _on_language_selected(index : int) -> void:
+	var language = LANGUAGES_DICTIONARY.values()[index]
+	SettingsData.on_language_changed(language)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	SettingsData.save_data()
+
+func update_texts():
+	$MarginContainer/VBoxContainer/Resolution/Label.text = tr("RESOLUTION")
+	$MarginContainer/VBoxContainer/Language/Label.text = tr("LANGUAGE")
--- Scripts/SettingsData.gd
+++ Scripts/SettingsData.gd
@@ -5,6 +5,7 @@ const SETTINGS_SAVE_PATH : String = "user://SettingsData.save"
 var settings_data_dict : Dictionary = {
 	"window_resolution_x" = "480",
 	"window_resolution_y" = "270",
+	"language" = "pt",
 	"master_volume" = "1.0",
 	"music_volume" = "1.0",
 	"sfx_volume" = "1.0"
@@ -27,6 +28,7 @@ func load_data():
 			var window_size = Vector2i(int(settings_data_dict["window_resolution_x"]),int(settings_data_dict["window_resolution_y"]))
 			DisplayServer.window_set_size(window_size)
 			get_window().move_to_center()
+		TranslationServer.set_locale(get_current_language())
 		AudioServer.set_bus_volume_db(0, linear_to_db(float(settings_data_dict["master_volume"]))) #Master
 		AudioServer.set_bus_volume_db(1, linear_to_db(float(settings_data_dict["music_volume"]))) #Music
 		AudioServer.set_bus_volume_db(2, linear_to_db(float(settings_data_dict["sfx_volume"]))) #SFX
@@ -34,6 +36,10 @@ func load_data():
 func on_resolution_changed(resolution : Vector2i):
 	settings_data_dict["window_resolution_x"] = str(resolution.x)
 	settings_data_dict["window_resolution_y"] = str(resolution.y)
+
+func on_language_changed(language : String):
+	settings_data_dict["language"] = language
+	TranslationServer.set_locale(language)
 
 func on_volume_changed(bus_name : String, value):
 	match bus_name:
@@ -43,3 +49,6 @@ func on_volume_changed(bus_name : String, value):
 			settings_data_dict["music_volume"] = str(value)
 		"SFX":
 			settings_data_dict["sfx_volume"] = str(value)
+
+func get_current_language():
+	return settings_data_dict["language"]
